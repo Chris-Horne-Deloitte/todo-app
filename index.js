c9a7f55cfefe06e4a3ac0d0c8d48dc754ffc3422
Chris Horne <chrhorne@deloitte.com>
2025-01-27 09:58:25 -0700

    Added test and separated server into it's own file

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
+// index.js
 const express = require('express');
 const app = express();
 
 app.get('/todos', (req, res) => {
-  const todos = [
-    { id: 1, title: 'Buy milk' },
-    { id: 2, title: 'Walk the dog' },
-  ];
-  res.json(todos);
+  res.send('Hello World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,6 @@
+// server.js
+const app = require('./index');
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
